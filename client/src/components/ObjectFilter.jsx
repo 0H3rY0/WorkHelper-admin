921ab68fd7d3220e50eeabed4cb1830d3a28f4bd
Szymon <szymers12345@gmail.com>
2025-02-08 21:11:52 +0100

    client/add option: add, delete filter forms

---
+++ client/src/components/ObjectFilter.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+
+const ObjectFilter = ({ id = 0, setObjectFilters }) => {
+  const handleRemoveObjectFilter = (e) => {
+    e.preventDefault();
+    setObjectFilters((prev) => {
+      return prev.filter((item) => item.id !== id);
+    });
+  };
+
+  //   const [selectedFilters, setSelectedFilters] = useState({});
+  const [selectedValue, setSelectedValue] = useState("nazwa");
+  const [textInputValue, setTextInputValue] = useState("");
+  const [isSwitch, setIsSwitch] = useState(false);
+
+  const onSelectValue = (e) => {
+    setSelectedValue(e.target.value);
+  };
+
+  const onSwitch = () => {
+    setIsSwitch((prev) => !prev);
+  };
+
+  const onTextInputChange = (e) => {
+    setTextInputValue(e.target.value);
+  };
+
+  if (isSwitch) {
+    const finalFilter = {
+      [selectedValue]: textInputValue,
+    };
+
+    console.log(finalFilter);
+  }
+
+  //   const onInputChange = (e) => {
+  //     setSelectedFilters((prev) => {
+  //       const newFilter = {
+  //         [e.target.name]: e.target.value,
+  //       };
+  //       return newFilter;
+  //     });
+  //   };
+
+  //   const selectedFilters = {
+  //     ulica: "",
+  //     nazwa: "",
+  //     miejscowosc: "",
+  //   };
+
+  return (
+    <form className="w-full flex justify-center gap-4 mt-4" id={id}>
+      <select name="" id="" className="input w-1/5" onChange={onSelectValue}>
+        <option value="nazwa">nazwa</option>
+        <option value="ulica">ulica</option>
+        <option value="miejscowosc">miejscowosc</option>
+      </select>
+
+      <div className="flex flex-col justify-center items-center">
+        <label
+          htmlFor="checkConteins"
+          className="font-bold text-md text-slate-700"
+        >
+          Niezawiera/Zawiera
+        </label>
+        <label className="inline-flex items-center cursor-pointer mt-1">
+          <input
+            type="checkbox"
+            value=""
+            className="sr-only peer"
+            onChange={onSwitch}
+          />
+          <div className="relative w-11 h-6 bg-gray-300 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600 dark:peer-checked:bg-green-600"></div>
+        </label>
+      </div>
+
+      <input type="text" className="input w-1/5" onChange={onTextInputChange} />
+
+      <button
+        type="button"
+        onClick={handleRemoveObjectFilter}
+        className="mt-4 text-red-500"
+      >
+        Usuń
+      </button>
+    </form>
+  );
+};
+
+export default ObjectFilter;
--- client/src/pages/Object.jsx
+++ client/src/pages/Object.jsx
@@ -1,51 +1,51 @@
 import { FaFilter } from "react-icons/fa6";
 import { CiCirclePlus } from "react-icons/ci";
 import { FaDatabase } from "react-icons/fa";
+import { v4 as uuidv4 } from "uuid"; // Importujemy uuid
+import ObjectFilter from "../components/ObjectFilter";
+import { useState } from "react";
 
 const Object = () => {
+  const [objectFilters, setObjectFilters] = useState([]);
+
+  const addObjectFilter = () => {
+    const newFilter = { id: uuidv4(), name: "new object" };
+    setObjectFilters((prev) => [...prev, newFilter]);
+  };
+
   return (
     <div className="w-full flex flex-col items-start p-14 ">
       <h2 className="font-semibold text-2xl text-slate-700 flex gap-2">
         Filtry <FaFilter size={32} />
       </h2>
-      <div className="w-full flex flex-col justify-center mt-5">
-        <form className="w-full flex justify-center gap-4">
-          <select name="" id="" className="input w-1/5">
-            <option value="nazwa">nazwa</option>
-            <option value="ulica">ulica</option>
-            <option value="miejscowosc">miejscowosc</option>
-          </select>
+      <div className="w-full flex flex-col  justify-center mt-5">
+        {objectFilters.map((item) => {
+          return (
+            <ObjectFilter
+              key={item.id}
+              id={item.id}
+              setObjectFilters={setObjectFilters}
+            />
+          );
+        })}
 
-          <div className="flex flex-col justify-center items-center">
-            <label
-              htmlFor="checkConteins"
-              className="font-bold text-md text-slate-700"
-            >
-              Niezawiera/Zawiera
-            </label>
-            <label className="inline-flex items-center cursor-pointer mt-1">
-              <input type="checkbox" value="" className="sr-only peer" />
-              <div className="relative w-11 h-6 bg-gray-300 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600 dark:peer-checked:bg-green-600"></div>
-            </label>
-          </div>
-
-          <input type="text" className="input w-1/5" />
-        </form>
-        <p type="submit" className="flex justify-center ">
+        <p type="submit" className="flex justify-center">
           <CiCirclePlus
             size={45}
             className="text-slate-700 cursor-pointer mt-10 hover:text-slate-500 scale-transition"
+            onClick={addObjectFilter}
           />
         </p>
       </div>
+
       <div className="w-full font-semibold text-2xl text-slate-700 flex gap-2 mt-20 justify-between items-center">
         <h2 className="flex gap-2">
           Dane <FaDatabase size={32} />
         </h2>
-        <button className=" button ">Pokaz</button>
+        <button className="button">Pokaz</button>
       </div>
 
-      {/* table */}
+      {/* Tabela */}
       <table className="table mt-7">
         <thead>
           <tr>
