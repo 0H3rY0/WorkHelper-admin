0de6e3fbfc2d650f448566b66d320a946228b023
Szymon <szymers12345@gmail.com>
2025-02-08 17:26:34 +0100

    client/maint template with static navbar and header

---
+++ client/src/components/layouts/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => (
+  <div className="col-span-2 bg-gray-700 p-4 text-white">Header</div>
+);
+
+export default Header;
---
+++ client/src/components/layouts/Navbar.jsx
@@ -0,0 +1,7 @@
+const Navbar = () => (
+  <div className="row-span-2 bg-gray-700 p-4 text-white hidden md:block">
+    Navbar
+  </div>
+);
+
+export default Navbar;
