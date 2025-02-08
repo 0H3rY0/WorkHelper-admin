3525dadc7248c96afe1bfe24eae24f170f8d28c5
Szymon <szymers12345@gmail.com>
2025-02-08 18:23:28 +0100

    client/devide navbar devices strucutre into more files

--- client/src/components/layouts/Navbar.jsx
+++ client/src/components/layouts/Navbar.jsx
@@ -1,16 +1,8 @@
 import { BiObjectsVerticalBottom } from "react-icons/bi";
 import { RiUserSearchLine } from "react-icons/ri";
-import { MdDevices } from "react-icons/md";
-import { FaArrowDown } from "react-icons/fa";
-import { FaArrowRight } from "react-icons/fa";
-import { FaLaptop } from "react-icons/fa";
-import { PiComputerTowerFill } from "react-icons/pi";
-import { BsCameraVideoFill } from "react-icons/bs";
-import { useState } from "react";
+import DevicesNavigation from "../ui/DevicesNavigation";
 
 const Navbar = () => {
-  const [openList, setOpenList] = useState(false);
-
   return (
     <div
       className="row-span-2 bg-gray-700 p-6 hidden md:block text-xl 
@@ -25,31 +17,7 @@ const Navbar = () => {
         </p>
 
         <hr className="rounded-lg border-2 border-slate-500" />
-
-        <div className="flex flex-col gap-2 ">
-          <p
-            className="flex items-center gap-2 cursor-pointer hover:text-slate-400"
-            onClick={() => setOpenList((prev) => !prev)}
-          >
-            <MdDevices size={32} /> Urzadzenia
-            {openList ? <FaArrowRight size={16} /> : <FaArrowDown size={16} />}
-          </p>
-          <ul
-            className={`${
-              openList ? "block" : "hidden"
-            } flex flex-col gap-1 mt-2`}
-          >
-            <li className="cursor-pointer ml-4 text-lg font-normal flex items-center gap-1 hover:text-slate-400">
-              <FaLaptop size={16} /> Laptopy
-            </li>
-            <li className="cursor-pointer ml-4 text-lg font-normal flex items-center gap-1 hover:text-slate-400">
-              <PiComputerTowerFill size={16} /> PC
-            </li>
-            <li className="cursor-pointer ml-4 text-lg font-normal flex items-center gap-1 hover:text-slate-400">
-              <BsCameraVideoFill size={16} /> Kamery
-            </li>
-          </ul>
-        </div>
+        <DevicesNavigation />
       </div>
     </div>
   );
