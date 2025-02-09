252b0c5f9015c8e697972e9f5e809742af52229c
Szymon <szymers12345@gmail.com>
2025-02-09 22:42:52 +0100

    client/ObjectPage refactor

--- client/src/pages/ObjectPage.jsx
+++ client/src/pages/ObjectPage.jsx
@@ -1,49 +1,13 @@
-import { FaFilter } from "react-icons/fa6";
-import { CiCirclePlus } from "react-icons/ci";
 import { FaDatabase } from "react-icons/fa";
-import { v4 as uuidv4 } from "uuid";
-import ObjectFilter from "../components/ObjectFilter";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router";
-import { FaSortAlphaUp } from "react-icons/fa";
-import { FaSortAlphaDown } from "react-icons/fa";
-import { FaSort } from "react-icons/fa";
+import ObjectsFilterSection from "../components/ObjectsFilterSection";
+import ObjectDataTable from "../components/ObjectDataTable";
 
 const ObjectPage = () => {
   const [objectFilters, setObjectFilters] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
   const [objectColumns, setObjectColumns] = useState([]);
-  const navigate = useNavigate();
-
-  const [sortConfig, setSortConfig] = useState({
-    column: null,
-    direction: "asc",
-  });
-
-  const sortDataByChoosenRecord = (chosenColumn) => {
-    setSortConfig((prevSortConfig) => {
-      const newDirection =
-        prevSortConfig.column === chosenColumn &&
-        prevSortConfig.direction === "asc"
-          ? "desc"
-          : "asc";
-      return {
-        column: chosenColumn,
-        direction: newDirection,
-      };
-    });
-
-    const sortedData = [...filteredData].sort((a, b) => {
-      if (sortConfig.direction === "asc") {
-        return a[chosenColumn] > b[chosenColumn] ? 1 : -1;
-      } else {
-        return a[chosenColumn] < b[chosenColumn] ? 1 : -1;
-      }
-    });
-
-    setFilteredData(sortedData);
-  };
 
   useEffect(() => {
     const getColumns = async () => {
@@ -53,11 +17,6 @@ const ObjectPage = () => {
 
     getColumns();
   }, []);
-
-  const addObjectFilter = () => {
-    const newFilter = { id: uuidv4(), name: "new object" };
-    setObjectFilters((prev) => [...prev, newFilter]);
-  };
 
   const getAllFilters = async () => {
     const filters = objectFilters?.length
@@ -79,29 +38,11 @@ const ObjectPage = () => {
 
   return (
     <div className="w-full flex flex-col items-start md:p-14 p-5 ">
-      <h2 className="font-semibold text-2xl text-slate-700 flex gap-2">
-        Filtry <FaFilter size={32} />
-      </h2>
-      <div className="w-full flex flex-col  justify-center mt-5">
-        {objectFilters.map((item) => {
-          return (
-            <ObjectFilter
-              key={item.id}
-              id={item.id}
-              setObjectFilters={setObjectFilters}
-              objectColumns={objectColumns}
-            />
-          );
-        })}
-
-        <p type="submit" className="flex justify-center">
-          <CiCirclePlus
-            size={45}
-            className="text-slate-700 cursor-pointer mt-10 hover:text-slate-500 scale-transition"
-            onClick={addObjectFilter}
-          />
-        </p>
-      </div>
+      <ObjectsFilterSection
+        objectFilters={objectFilters}
+        setObjectFilters={setObjectFilters}
+        objectColumns={objectColumns}
+      />
 
       <div className="w-full font-semibold text-2xl text-slate-700 flex gap-2 mt-20 justify-between items-center">
         <h2 className="flex gap-2">
@@ -115,59 +56,11 @@ const ObjectPage = () => {
         </button>
       </div>
 
-      <table className="table mt-7">
-        <thead>
-          <tr>
-            {objectColumns.map((item) => (
-              <th key={item}>
-                <div className="flex items-center gap-2">
-                  <p>{item}</p>
-
-                  {/* Pokazujemy tylko jedną ikonę w zależności od sortowania */}
-                  {sortConfig.column === item ? (
-                    sortConfig.direction === "asc" ? (
-                      <FaSortAlphaUp
-                        onClick={() => sortDataByChoosenRecord(item)}
-                        className="text-slate-400 hover:text-slate-500 hover:scale-110 scale-transition"
-                        size={18}
-                      />
-                    ) : (
-                      <FaSortAlphaDown
-                        onClick={() => sortDataByChoosenRecord(item)}
-                        className="text-slate-400 hover:text-slate-500 hover:scale-110 scale-transition"
-                        size={18}
-                      />
-                    )
-                  ) : (
-                    <FaSort
-                      onClick={() => sortDataByChoosenRecord(item)}
-                      className="text-slate-400 hover:text-slate-500 hover:scale-110 scale-transition"
-                      size={18}
-                    />
-                  )}
-                </div>
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData.length > 0 ? (
-            filteredData.map((item) => (
-              <tr key={item.id} onClick={() => navigate(`object/${item.id}`)}>
-                {objectColumns.map((col, index) => (
-                  <td key={index} data-label={col}>
-                    {item[col]}
-                  </td>
-                ))}
-              </tr>
-            ))
-          ) : (
-            <tr className="sm:text-center no-results">
-              <td colSpan="100%">No results</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+      <ObjectDataTable
+        objectColumns={objectColumns}
+        setFilteredData={setFilteredData}
+        filteredData={filteredData}
+      />
     </div>
   );
 };
