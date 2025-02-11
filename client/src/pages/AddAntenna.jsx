cb6b52197305be582b0157c6ea1d7ea313c1fbf3
Szymon <szymers12345@gmail.com>
2025-02-11 17:59:19 +0100

    client/antennaPage frontend

---
+++ client/src/pages/AddAntenna.jsx
@@ -0,0 +1,46 @@
+// import { useState } from "react";
+import { IoMdAdd } from "react-icons/io";
+// import axios from "axios";
+import AddDeviceForm from "../components/AddDeviceForm";
+import useAddDeviceForm from "../hooks/useAddDeviceForm";
+import { antennaFields } from "../utils/deviceFormFilds/antennaFields";
+
+const AddLaptopPage = () => {
+  const initialAntennaState = {
+    czasza: 0,
+    antena_dvbt: 0,
+    antena_radiowa: 0,
+    zwrotnica: 0,
+    ilosc_rozgaleznikow: 0,
+    uwagi: "",
+    notatki: "",
+    dataOD: "",
+    dataDO: "",
+  };
+
+  const { formState, handleChange, handleSubmit, error } = useAddDeviceForm(
+    initialAntennaState,
+    "http://localhost:3000/api/laptop/add"
+  );
+
+  return (
+    <div className="w-full p-7 flex flex-col items-start justify-start">
+      <div className="w-full">
+        <h2 className="h2 flex items-center gap-2 mb-10 text-xl md:text-2xl">
+          Add Laptop <IoMdAdd size={32} />
+        </h2>
+        <AddDeviceForm
+          handleSubmitForm={handleSubmit}
+          fields={antennaFields}
+          onInputChange={handleChange}
+          DeviceState={formState}
+        />
+        <p className="text-[1rem] text-red-400 mt-5 font-semibold">
+          {error ? error : null}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AddLaptopPage;
