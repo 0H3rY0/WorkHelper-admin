fff0d9d57cc34dc30ea3d029f10b187895476c1b
Szymon <szymers12345@gmail.com>
2025-02-11 18:23:56 +0100

    addNVRPage full frontend

---
+++ client/src/pages/AddNVR.jsx
@@ -0,0 +1,54 @@
+// import { useState } from "react";
+import { IoMdAdd } from "react-icons/io";
+// import axios from "axios";
+import AddDeviceForm from "../components/AddDeviceForm";
+import useAddDeviceForm from "../hooks/useAddDeviceForm";
+import { nvrFields } from "../utils/deviceFormFilds/nvrFields";
+
+const AddNVRPage = () => {
+  const initialNVRState = {
+    nr_seryjny: "",
+    model: "",
+    mac: "",
+    podzial_uprawnien: 0,
+    model_dysku: "",
+    wielkoscDysku: 0,
+    dataWymianyDysku: "",
+    portHTTP: 0,
+    portDANE: 0,
+    iloscKamer: 0,
+    ipWewnetrzne: "",
+    ipZewnetrzne: "",
+    p2p: 0,
+    uwagi: "",
+    notatki: "",
+    dataOD: "",
+    dataDO: "",
+  };
+
+  const { formState, handleChange, handleSubmit, error } = useAddDeviceForm(
+    initialNVRState,
+    "http://localhost:3000/api/laptop/add"
+  );
+
+  return (
+    <div className="w-full p-7 flex flex-col items-start justify-start">
+      <div className="w-full">
+        <h2 className="h2 flex items-center gap-2 mb-10 text-xl md:text-2xl">
+          Add Laptop <IoMdAdd size={32} />
+        </h2>
+        <AddDeviceForm
+          handleSubmitForm={handleSubmit}
+          fields={nvrFields}
+          onInputChange={handleChange}
+          DeviceState={formState}
+        />
+        <p className="text-[1rem] text-red-400 mt-5 font-semibold">
+          {error ? error : null}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AddNVRPage;
