d0e64e0372729ba45e06f47ed5a4b363049b27b7
Szymon <szymers12345@gmail.com>
2025-02-11 16:49:06 +0100

    AddLaptopPage refactor

--- client/src/pages/AddLaptopPage.jsx
+++ client/src/pages/AddLaptopPage.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { IoMdAdd } from "react-icons/io";
-import axios from "axios";
+// import axios from "axios";
+import AddDeviceForm from "../components/AddDeviceForm";
+import useAddDeviceForm from "../hooks/useAddDeviceForm";
+import { laptopFields } from "../utils/deviceFormFilds/laptopFields";
 
 const AddLaptopPage = () => {
   const initialLaptopState = {
@@ -9,48 +12,23 @@ const AddLaptopPage = () => {
     podzial_uprawnien: 0,
     system_operacyjny: "",
     rodzaj_dysku: "",
-    data_wymiany_dysku: null,
+    data_wymiany_dysku: "",
     ram: 0,
     karta_graficzna: 0,
     plyta_glowna: "",
     zasilacz: "",
-    program_zdalny: null,
-    id_programu: null,
-    uwagi: null,
-    notatki: null,
-    data_od: null,
-    data_do: null,
+    program_zdalny: "",
+    id_programu: "",
+    uwagi: "",
+    notatki: "",
+    data_od: "",
+    data_do: "",
   };
 
-  const [laptopState, setLaptopState] = useState(initialLaptopState);
-  const [error, setError] = useState(null);
-
-  const onInputChange = (e) => {
-    const { name, type, checked, value } = e.target;
-
-    setLaptopState((prev) => ({
-      ...prev,
-      [name]: type === "checkbox" ? (checked ? 1 : 0) : value,
-    }));
-
-    console.log(laptopState);
-  };
-
-  const handleSubmitForm = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await axios.post(
-        "http://localhost:3000/api/laptop/add",
-        laptopState
-      );
-
-      console.log(response);
-    } catch (error) {
-      console.log(error.response.data.error);
-      setError(error.response.data.error);
-    }
-  };
+  const { formState, handleChange, handleSubmit, error } = useAddDeviceForm(
+    initialLaptopState,
+    "http://localhost:3000/api/laptop/add"
+  );
 
   return (
     <div className="w-full p-7 flex flex-col items-start justify-start">
@@ -58,35 +36,12 @@ const AddLaptopPage = () => {
         <h2 className="h2 flex items-center gap-2 mb-10 text-xl md:text-2xl">
           Add Laptop <IoMdAdd size={32} />
         </h2>
-
-        <form
-          onSubmit={(e) => handleSubmitForm(e)}
-          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full"
-        >
-          {fields.map((field) => (
-            <div key={field.id} className="flex flex-col gap-1">
-              <label
-                htmlFor={field.id}
-                className="text-sm font-medium text-custom-blue"
-              >
-                {field.label}
-              </label>
-              <input
-                id={field.id}
-                type={field.type}
-                name={field.name}
-                onChange={onInputChange}
-                className="p-2 border border-custom-blue rounded-md bg-dark-gray text-white focus:outline-none focus:ring-2 focus:ring-custom-blue"
-              />
-            </div>
-          ))}
-          <button
-            type="submit"
-            className="button bg-custom-blue text-white mt-10 w-full"
-          >
-            Add
-          </button>
-        </form>
+        <AddDeviceForm
+          handleSubmitForm={handleSubmit}
+          fields={laptopFields}
+          onInputChange={handleChange}
+          DeviceState={formState}
+        />
         <p className="text-[1rem] text-red-400 mt-5 font-semibold">
           {error ? error : null}
         </p>
@@ -94,64 +49,5 @@ const AddLaptopPage = () => {
     </div>
   );
 };
-
-const fields = [
-  { id: "nrSeryjny", label: "Nr Seryjny", type: "text", name: "nr_seryjny" },
-  { id: "model", label: "Model", type: "text", name: "model" },
-  {
-    id: "system",
-    label: "System Operacyjny",
-    type: "text",
-    name: "system_operacyjny",
-  },
-  {
-    id: "uprawnienia",
-    label: "Uprawnienia",
-    type: "checkbox",
-    name: "podzial_uprawnien",
-  },
-  {
-    id: "rodzajDysku",
-    label: "Rodzaj Dysku",
-    type: "text",
-    name: "rodzaj_dysku",
-  },
-  {
-    id: "dataWymianyDysku",
-    label: "Data Wymiany Dysku",
-    type: "date",
-    name: "data_wymiany_dysku",
-  },
-  { id: "ram", label: "RAM", type: "number", name: "ram" },
-  {
-    id: "kartaGraficzna",
-    label: "Karta Graficzna (zintegrowana)",
-    type: "checkbox",
-    name: "karta_graficzna",
-  },
-  {
-    id: "plytaGlowna",
-    label: "Płyta Główna",
-    type: "text",
-    name: "plyta_glowna",
-  },
-  { id: "zasilacz", label: "Zasilacz", type: "text", name: "zasilacz" },
-  {
-    id: "programZdalny",
-    label: "Program Zdalny",
-    type: "text",
-    name: "program_zdalny",
-  },
-  {
-    id: "idProgramu",
-    label: "ID Programu",
-    type: "text",
-    name: "id_programu",
-  },
-  { id: "uwagi", label: "Uwagi", type: "text", name: "uwagi" },
-  { id: "notatki", label: "Notatki", type: "text", name: "notatki" },
-  { id: "dataDo", label: "Data do", type: "date", name: "data_do" },
-  { id: "dataOd", label: "Data od", type: "date", name: "data_od" },
-];
 
 export default AddLaptopPage;
