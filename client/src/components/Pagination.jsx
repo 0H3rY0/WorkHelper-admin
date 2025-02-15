7fcd977b98e68b7fa6da15869d43817b5c29c43f
Szymon <szymers12345@gmail.com>
2025-02-15 19:18:54 +0100

    pagination display logic changed

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -7,28 +7,28 @@ const Pagination = ({
   renderPageNumbers,
 }) => {
   return (
-    totalPages > 1 && (
-      <div className="w-full flex justify-center items-center bg-dark-gray p-2">
-        {currentPage > 1 && (
-          <button
-            onClick={() => changePage(currentPage - 1)}
-            className="px-4 py-2 rounded-full bg-gray-300 hover:bg-gray-400"
-          >
-            <FaChevronLeft />
-          </button>
-        )}
-        {renderPageNumbers()}
-        {currentPage < totalPages && (
-          <button
-            onClick={() => changePage(currentPage + 1)}
-            className="px-4 py-2 rounded-full bg-gray-300 hover:bg-gray-400"
-          >
-            <FaChevronRight />
-          </button>
-        )}
-      </div>
-    )
+    // totalPages > 1 && (
+    <div className="w-full flex justify-center items-center bg-dark-gray p-2 min-h-12">
+      {currentPage > 1 && (
+        <button
+          onClick={() => changePage(currentPage - 1)}
+          className="px-4 py-2 rounded-full bg-gray-300 hover:bg-gray-400"
+        >
+          <FaChevronLeft />
+        </button>
+      )}
+      {renderPageNumbers()}
+      {currentPage < totalPages && (
+        <button
+          onClick={() => changePage(currentPage + 1)}
+          className="px-4 py-2 rounded-full bg-gray-300 hover:bg-gray-400"
+        >
+          <FaChevronRight />
+        </button>
+      )}
+    </div>
   );
+  // );
 };
 
 export default Pagination;
