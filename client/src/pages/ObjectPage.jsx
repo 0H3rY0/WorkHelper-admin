4bbdb491eb74e5a9d3051e979775b594c7816412
Szymon <szymers12345@gmail.com>
2025-02-15 18:29:29 +0100

    add hook useFilters and components Pagination and Filters(this name will be chenged)

--- client/src/pages/ObjectPage.jsx
+++ client/src/pages/ObjectPage.jsx
@@ -1,19 +1,27 @@
-import { FaDatabase, FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { useState, useEffect } from "react";
-import axios from "axios";
+import useFilters from "../hooks/useFilters";
 import ObjectsFilterSection from "../components/ObjectsFilterSection";
 import ObjectDataTable from "../components/ObjectDataTable";
+import Filters from "../components/Filters";
+import Pagination from "../components/Pagination";
+import axios from "axios";
 
 const ObjectPage = () => {
-  const [objectFilters, setObjectFilters] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
-  const [objectColumns, setObjectColumns] = useState([]);
-  const [originalData, setOriginalData] = useState([]);
-  const [rowLimit, setRowLimit] = useState(5);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-
   const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+  const {
+    objectFilters,
+    setObjectFilters,
+    setFilteredData,
+    filteredData,
+    rowLimit,
+    getAllFilters,
+    changeFilteredDataRowsLimit,
+    currentPage,
+    setCurrentPage,
+    totalPages,
+  } = useFilters(BACKEND_URL);
+
+  const [objectColumns, setObjectColumns] = useState([]);
 
   useEffect(() => {
     const getColumns = async () => {
@@ -23,48 +31,6 @@ const ObjectPage = () => {
 
     getColumns();
   }, []);
-
-  const getAllFilters = async () => {
-    const filters = objectFilters?.length
-      ? objectFilters
-          .map((item) => item.name)
-          .reduce((acc, item) => ({ ...acc, ...item }), {})
-      : {};
-
-    try {
-      const response = await axios.post(`${BACKEND_URL}/api/objects`, {
-        filters,
-      });
-
-      setOriginalData(response.data);
-      const newTotalPages = Math.max(
-        1,
-        Math.ceil(response.data.length / rowLimit)
-      );
-      setTotalPages(newTotalPages);
-      setCurrentPage(1);
-      setFilteredData(response.data.slice(0, rowLimit));
-    } catch (error) {
-      console.error("Błąd podczas pobierania danych:", error);
-    }
-  };
-
-  const changeFilteredDataRowsLimit = (e) => {
-    const limit = Number(e.target.value);
-    setRowLimit(limit);
-    const newTotalPages = Math.max(1, Math.ceil(originalData.length / limit));
-    setTotalPages(newTotalPages);
-    setCurrentPage(1);
-    setFilteredData(originalData.slice(0, limit));
-  };
-
-  const changePage = (page) => {
-    if (page < 1 || page > totalPages) return;
-    setCurrentPage(page);
-    const startIndex = (page - 1) * rowLimit;
-    const endIndex = startIndex + rowLimit;
-    setFilteredData(originalData.slice(startIndex, endIndex));
-  };
 
   const renderPageNumbers = () => {
     if (totalPages <= 1) return null;
@@ -80,7 +46,7 @@ const ObjectPage = () => {
           className={`px-3 py-1 m-1 rounded-full ${
             i === currentPage ? "bg-custom-blue text-white" : "bg-gray-200"
           }`}
-          onClick={() => changePage(i)}
+          onClick={() => setCurrentPage(i)}
         >
           {i}
         </button>
@@ -98,59 +64,26 @@ const ObjectPage = () => {
         objectColumns={objectColumns}
       />
 
-      <div className="w-full font-semibold text-2xl text-slate-700 flex sm:flex-row flex-col gap-2 mt-20 sm:justify-between sm:items-center px-7">
-        <h2 className="flex gap-2">
-          Dane <FaDatabase size={32} />
-        </h2>
-        <div className="flex items-center justify-start gap-10">
-          <div className="flex items-center justify-center gap-2 text-[1rem]">
-            <select
-              className="bg-white hover:bg-slate-400 shadow-xl rounded-lg p-2 hover:scale-110 scale-transition"
-              onChange={changeFilteredDataRowsLimit}
-              value={rowLimit}
-            >
-              <option value={5}>5</option>
-              <option value={10}>10</option>
-              <option value={20}>20</option>
-            </select>
-            <p>Liczba wyświetleń</p>
-          </div>
-          <button
-            className="button bg-custom-blue text-white"
-            onClick={() => getAllFilters()}
-          >
-            Pokaż
-          </button>
-        </div>
-      </div>
+      <Filters
+        getAllFilters={getAllFilters}
+        objectFilters={objectFilters}
+        setObjectFilters={setObjectFilters}
+        rowLimit={rowLimit}
+        changeFilteredDataRowsLimit={changeFilteredDataRowsLimit}
+      />
 
       <ObjectDataTable
         objectColumns={objectColumns}
-        setFilteredData={setFilteredData}
         filteredData={filteredData}
+        setFilteredData={setFilteredData}
       />
 
-      {totalPages > 1 && (
-        <div className="w-full flex justify-center items-center bg-dark-gray p-2">
-          {currentPage > 1 && (
-            <button
-              onClick={() => changePage(currentPage - 1)}
-              className="px-4 py-2 rounded-full bg-gray-300 hover:bg-gray-400"
-            >
-              <FaChevronLeft />
-            </button>
-          )}
-          {renderPageNumbers()}
-          {currentPage < totalPages && (
-            <button
-              onClick={() => changePage(currentPage + 1)}
-              className="px-4 py-2 rounded-full bg-gray-300 hover:bg-gray-400"
-            >
-              <FaChevronRight />
-            </button>
-          )}
-        </div>
-      )}
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        changePage={setCurrentPage}
+        renderPageNumbers={renderPageNumbers}
+      />
     </div>
   );
 };
