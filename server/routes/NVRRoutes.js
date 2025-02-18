42cb8cca9db44d7abfc0d693709eb31bc73e74bd
Szymon <szymers12345@gmail.com>
2025-02-18 15:55:25 +0100

    every item endpoints

--- server/routes/NVRRoutes.js
+++ server/routes/NVRRoutes.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const { addNvr } = require("../controllers/NVRController");
 
+const {
+  getColumns,
+  getTableRecords,
+  getRecordById,
+  deleteRecord,
+  editItem,
+} = require("../controllers/itemController");
+
 const router = express.Router();
 
 router.post("/nvr/add", addNvr);
+
+const tableName = "nvr";
+
+router.get("/nvr/columns", (req, res) =>
+  getColumns({ ...req, params: { tableName } }, res)
+);
+router.post("/nvr/table-records", (req, res) =>
+  getTableRecords({ ...req, params: { tableName } }, res)
+);
+router.get("/nvr/:id", (req, res) =>
+  getRecordById({ ...req, params: { tableName, id: req.params.id } }, res)
+);
+router.post("/nvr/delete", (req, res) =>
+  deleteRecord({ ...req, params: { tableName } }, res)
+);
+
+router.post("/nvr/edit", (req, res) =>
+  editItem({ ...req, params: { tableName } }, res)
+);
 
 module.exports = router;
--- server/routes/remainingRoutes.js
+++ server/routes/remainingRoutes.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const { addRemaining } = require("../controllers/remainingController");
 
+const {
+  getColumns,
+  getTableRecords,
+  getRecordById,
+  deleteRecord,
+  editItem,
+} = require("../controllers/itemController");
+
 const router = express.Router();
 
 router.post("/remaining/add", addRemaining);
+
+const tableName = "pozostale";
+
+router.get("/pozostale/columns", (req, res) =>
+  getColumns({ ...req, params: { tableName } }, res)
+);
+router.post("/pozostale/table-records", (req, res) =>
+  getTableRecords({ ...req, params: { tableName } }, res)
+);
+router.get("/pozostale/:id", (req, res) =>
+  getRecordById({ ...req, params: { tableName, id: req.params.id } }, res)
+);
+router.post("/pozostale/delete", (req, res) =>
+  deleteRecord({ ...req, params: { tableName } }, res)
+);
+
+router.post("/pozostale/edit", (req, res) =>
+  editItem({ ...req, params: { tableName } }, res)
+);
 
 module.exports = router;
