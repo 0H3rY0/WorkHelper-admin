c1e6833d91128cb5b6b5c87c81cbe50b87b0fb41
Szymon <szymers12345@gmail.com>
2025-02-18 22:20:03 +0100

    AddDeviceForm refactor

--- client/src/components/AddDeviceForm.jsx
+++ client/src/components/AddDeviceForm.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
-import axios from "axios"; // Do pobierania danych z backendu
+import { useState } from "react";
+import useFetchOptions from "../hooks/useFetchOptions";
+import FormField from "../components/FormField";
 
 const AddDeviceForm = ({
   fields,
@@ -7,44 +8,14 @@ const AddDeviceForm = ({
   onInputChange,
   DeviceState,
 }) => {
+  const options = useFetchOptions(fields);
+
   const [inputModes, setInputModes] = useState(
     fields.reduce((acc, field) => {
       if (field.type === "select") acc[field.name] = "select";
       return acc;
     }, {})
   );
-
-  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-
-  const [options, setOptions] = useState({}); // Przechowuje dane z bazy
-
-  useEffect(() => {
-    const fetchOptions = async () => {
-      try {
-        const responses = await Promise.all(
-          fields
-            .filter((field) => field.type === "select" && field.endpoint)
-            .map((field) =>
-              axios
-                .get(`${BACKEND_URL}/${field.endpoint}`)
-                .then((res) => ({ name: field.name, data: res.data }))
-            )
-        );
-
-        const newOptions = responses.reduce((acc, { name, data }) => {
-          acc[name] = data;
-          return acc;
-        }, {});
-
-        setOptions(newOptions);
-        console.log(newOptions);
-      } catch (error) {
-        console.error("Błąd pobierania danych:", error);
-      }
-    };
-
-    fetchOptions();
-  }, [fields]);
 
   const handleModeChange = (fieldName, mode) => {
     setInputModes((prevModes) => ({ ...prevModes, [fieldName]: mode }));
@@ -54,66 +25,15 @@ const AddDeviceForm = ({
     <form onSubmit={handleSubmitForm} className="w-full flex flex-col">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full">
         {fields.map((field) => (
-          <div key={field.id} className="flex flex-col gap-1">
-            <label
-              htmlFor={field.id}
-              className="text-sm font-medium text-custom-blue"
-            >
-              {field.label}
-            </label>
-
-            {field.type === "select" ? (
-              <>
-                <select
-                  id={field.id}
-                  className="input p-2 border border-custom-blue rounded-md bg-dark-gray text-white focus:outline-none focus:ring-2 focus:ring-custom-blue"
-                  name={field.name}
-                  onChange={(e) => handleModeChange(field.name, e.target.value)}
-                  value={inputModes[field.name]}
-                >
-                  <option value="select">Wybór z listy</option>
-                  <option value="manual">Wpisanie ręczne</option>
-                </select>
-
-                {inputModes[field.name] === "select" ? (
-                  <select
-                    className="input p-2 border border-custom-blue rounded-md bg-dark-gray text-white focus:outline-none focus:ring-2 focus:ring-custom-blue"
-                    name={field.name}
-                    onChange={onInputChange}
-                    value={DeviceState[field.name] || ""}
-                  >
-                    <option value="">Wybierz...</option>
-                    {options[field.name]?.map((opt) => (
-                      <option key={opt.id} value={opt.id}>
-                        {opt.label ||
-                          opt.name ||
-                          opt.nazwa ||
-                          opt.imie + " " + opt.nazwisko}
-                      </option>
-                    ))}
-                  </select>
-                ) : (
-                  <input
-                    id={field.id}
-                    type="number"
-                    name={field.name}
-                    onChange={onInputChange}
-                    value={DeviceState[field.name] || ""}
-                    className="p-2 border border-custom-blue rounded-md bg-dark-gray text-white focus:outline-none focus:ring-2 focus:ring-custom-blue"
-                  />
-                )}
-              </>
-            ) : (
-              <input
-                value={DeviceState[field.name] || ""}
-                id={field.id}
-                type={field.type}
-                name={field.name}
-                onChange={onInputChange}
-                className="p-2 border border-custom-blue rounded-md bg-dark-gray text-white focus:outline-none focus:ring-2 focus:ring-custom-blue"
-              />
-            )}
-          </div>
+          <FormField
+            key={field.id}
+            field={field}
+            onInputChange={onInputChange}
+            DeviceState={DeviceState}
+            inputModes={inputModes}
+            handleModeChange={handleModeChange}
+            options={options}
+          />
         ))}
       </div>
 
