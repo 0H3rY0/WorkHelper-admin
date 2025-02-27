1bf5e38b7843a697b8328c3002f83776c060fcb7
Szymon <szymers12345@gmail.com>
2025-02-27 13:11:01 +0100

    changed index.js

--- server/index.js
+++ server/index.js
@@ -39,10 +39,9 @@ app.use("/api", usersRoutes);
 app.use("/api", clientRoutes);
 app.use("/api/raport", raportRoutes);
 
-app.use(express.static(path.join(__dirname, "client", "dist"))); // Zmiana na Vite
-
+app.use(express.static(path.join(__dirname, "../client/dist"))); // Cofnij się o jeden katalog
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
